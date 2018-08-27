7e61f6500b428932b9c01ab23b1c7cb1b29278bc
HH <42507493+HH283599198@users.noreply.github.com>
2018-08-27 19:42:06 +0800

    Add files via upload

---
+++ test/src/Main.java
@@ -0,0 +1,96 @@
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("lambda表达式");
+
+
+//        //遍历
+        String[] po = {"H4","H2",
+                "H3","H5","H6","H7","H1",
+           };
+        //排序
+        Comparator<String> sortByName = (String s1, String s2) -> (s1.compareTo(s2));
+        Arrays.sort(po, sortByName);
+        Arrays.sort(po, (String s1, String s2) -> (s1.compareTo(s2)));
+        List<String> hs =  Arrays.asList(po);
+
+        hs.forEach((h) -> System.out.print(h + "; "));
+        //双冒号写法
+        hs.forEach(System.out::println);
+
+        List<Person> javaProgrammers = new ArrayList<Person>() {
+            {
+                add(new Person("Elsdon", "Jaycob", "Java programmer", "male", 43, 2000));
+                add(new Person("Tamsen", "Brittany", "Java programmer", "female", 23, 1500));
+                add(new Person("Floyd", "Donny", "Java programmer", "male", 33, 1800));
+                add(new Person("Sindy", "Jonie", "Java programmer", "female", 32, 1600));
+                add(new Person("Vere", "Hervey", "Java programmer", "male", 22, 1200));
+                add(new Person("Maude", "Jaimie", "Java programmer", "female", 27, 1900));
+                add(new Person("Shawn", "Randall", "Java programmer", "male", 30, 2300));
+                add(new Person("Jayden", "Corrina", "Java programmer", "female", 35, 1700));
+                add(new Person("Palmer", "Dene", "Java programmer", "male", 33, 2000));
+                add(new Person("Addison", "Pam", "Java programmer", "female", 34, 1300));
+            }
+        };
+
+        List<Person> phpProgrammers = new ArrayList<Person>() {
+            {
+                add(new Person("Jarrod", "Pace", "PHP programmer", "male", 34, 1550));
+                add(new Person("Clarette", "Cicely", "PHP programmer", "female", 23, 1200));
+                add(new Person("Victor", "Channing", "PHP programmer", "male", 32, 1600));
+                add(new Person("Tori", "Sheryl", "PHP programmer", "female", 21, 1000));
+                add(new Person("Osborne", "Shad", "PHP programmer", "male", 32, 1100));
+                add(new Person("Rosalind", "Layla", "PHP programmer", "female", 25, 1300));
+                add(new Person("Fraser", "Hewie", "PHP programmer", "male", 36, 1100));
+                add(new Person("Quinn", "Tamara", "PHP programmer", "female", 21, 1000));
+                add(new Person("Alvin", "Lance", "PHP programmer", "male", 38, 1600));
+                add(new Person("Evonne", "Shari", "PHP programmer", "female", 40, 1800));
+            }
+        };
+
+
+        javaProgrammers.forEach((p) -> System.out.printf("%s %s; ", p.getFirstName(), p.getLastName()));
+        phpProgrammers.forEach((p) -> System.out.printf("%s %s; ", p.getFirstName(), p.getLastName()));
+
+        System.out.println("下面是月薪超过 $1,400 的PHP程序员:");
+        phpProgrammers.stream()
+                .filter((p) -> (p.getSalary() > 1400))
+                .forEach((p) -> System.out.printf("%s %s; ", p.getFirstName(), p.getLastName()));
+
+        // 定义 filters
+        Predicate<Person> ageFilter = (p) -> (p.getAge() > 25);
+        Predicate<Person> salaryFilter = (p) -> (p.getSalary() > 1400);
+        Predicate<Person> genderFilter = (p) -> ("female".equals(p.getGender()));
+
+        System.out.println("下面是年龄大于 24岁且月薪在$1,400以上的女PHP程序员:");
+        phpProgrammers.stream()
+                .filter(ageFilter)
+                .filter(salaryFilter)
+                .filter(genderFilter)
+                .forEach((p) -> System.out.printf("%s %s; ", p.getFirstName(), p.getLastName()));
+
+        // 重用filters
+        System.out.println("年龄大于 24岁的女性 Java programmers:");
+        javaProgrammers.stream()
+                .filter(ageFilter)
+                .filter(genderFilter)
+                .forEach((p) -> System.out.printf("%s %s; ", p.getFirstName(), p.getLastName()));
+
+    }
+}
+
+
+
+
---
+++ test/src/Person.java
@@ -0,0 +1,63 @@
+public class Person {
+
+    private String firstName, lastName, job, gender;
+    private int salary, age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName, String job,
+                  String gender, int age, int salary)       {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.job = job;
+        this.salary = salary;
+    }
+}
